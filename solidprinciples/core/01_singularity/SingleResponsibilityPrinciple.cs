5c0de58ca5d7acf100821153f16dd48b687f91de
3vilbird <devilisnotinhome@gmail.com>
2023-06-19 20:27:15 +0530

    basic structure

---
+++ solidprinciples/core/01_singularity/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,8 @@
+namespace solidprinciples.core
+{
+    // link https://www.c-sharpcorner.com/UploadFile/damubetha/solid-principles-in-C-Sharp/
+
+    public class SingleResponsibilityPrinciple
+    {
+    }
+}
